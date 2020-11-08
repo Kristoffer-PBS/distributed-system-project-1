7953757d9865fe1057a0691f9451e64f352246a1
Kristoffer Sørensen <kristoffer.pbs@gmail.com>
2020-11-08 20:05:09 +0100

    added check to handle negative numbers of noden given as argument

--- improved_bully_election/src/main.cpp
+++ improved_bully_election/src/main.cpp
@@ -8,8 +8,15 @@ int main(int argc, char *argv[]) {
         cout << print_red("invalid number of arguments " + std::to_string(argc) + " were given, but 1 is needed") << endl;
         return 1;
     }
+    
+    int number_of_nodes = std::atoi(argv[1]);
 
-    Network nw(std::atoi(argv[1]));
+    if (number_of_nodes < 0) {
+        cout << print_red("number of nodes cannot be negative") << endl;
+        return 1;
+    }
+
+    Network nw(number_of_nodes);
 
     nw.run(100);
 
